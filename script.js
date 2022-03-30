6ab3a403844fcae34d8141b09350716608ff4a0d
HussinSoli <74909933+HussinSoli@users.noreply.github.com>
2022-03-30 16:46:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+const div = document.querySelector("#root");
+const btn = document.querySelector(".btn");
+const span = document.querySelector(".quote--id");
+
+const renderQoute = (quote) => {
+  div.insertAdjacentText("beforeend", quote.slip.advice);
+  span.insertAdjacentText("beforeend", quote.slip.id);
+  div.style.opacity = 1;
+};
+
+const getQoute = () => {
+  div.style.opacity = 0;
+  span.textContent = "";
+  div.textContent = "";
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((quote) => {
+      renderQoute(quote);
+    });
+};
+// getQoute();
+btn.addEventListener("click", getQoute);
